c418ab29c324eb38b88a3d76f7faf0c23046a5d3
MauroLucas <m_l_pereyra@hotmail.com>
2019-06-24 23:07:12 -0300

    Adjunto clase Test de carga de datos

---
+++ Proyecto/BD2-TP/src/test/TestCargaDeDatos.java
@@ -0,0 +1,40 @@
+package test;
+
+import modelo.Domicilio;
+import modelo.Localidad;
+import modelo.Producto;
+import modelo.Provincia;
+
+public class TestCargaDeDatos {
+
+	public static void main(String[] args) {
+		//Localidades//
+		agregarLocalidad(new Localidad(1,"Adrogue"));
+		agregarLocalidad(new Localidad(2,"Lomas de Zamora"));
+		agregarLocalidad(new Localidad(3,"Temperley"));
+		agregarLocalidad(new Localidad(4,"Banfield"));
+		agregarLocalidad(new Localidad(5,"Lanus"));
+		
+		//Provincia//
+		agregarProvincia(new Provincia(1,"Buenos Aires"));
+		
+		//Productos//
+		
+
+	}
+	
+	
+	
+	public static void agregarProvincia(Provincia provincia) {
+		
+	}
+	
+	public static void agregarLocalidad(Localidad localidad) {
+		
+	}
+	
+	public static void agregarProducto(Producto producto) {
+		
+	}
+
+}
