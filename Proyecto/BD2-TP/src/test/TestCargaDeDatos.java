3ae0dbb016a67de5b4cfdde667c7f70615681ecc
MauroLucas <m_l_pereyra@hotmail.com>
2019-06-24 23:26:10 -0300

    conjunto de datos de productos->medicamentos

--- Proyecto/BD2-TP/src/test/TestCargaDeDatos.java
+++ Proyecto/BD2-TP/src/test/TestCargaDeDatos.java
@@ -19,7 +19,19 @@ public class TestCargaDeDatos {
 		agregarProvincia(new Provincia(1,"Buenos Aires"));
 		
 		//Productos//
-		
+		 //Perfumeria//
+		 
+		 //Medicamento//
+		 agregarProducto(new Producto(1,"medicamento","Omeprazol","Klonal",111,258));
+		 agregarProducto(new Producto(2,"medicamento","Clonazepam","Klonal",112,150));
+		 agregarProducto(new Producto(3,"medicamento","Ibuprofeno","Klonal",113,200));
+		 agregarProducto(new Producto(4,"medicamento","Buscapina","Bernabo",114,123));
+		 agregarProducto(new Producto(5,"medicamento","Sertal","Bernabo",115,240));
+		 agregarProducto(new Producto(6,"medicamento","Amoxicilina","Bernabo",116,100));
+		 agregarProducto(new Producto(7,"medicamento","Aspirina","Laboratorios Isa",117,78));
+		 agregarProducto(new Producto(8,"medicamento","Alprazolam","Laboratorios Isa",118,50));
+		 agregarProducto(new Producto(9,"medicamento","Paracetamol","Laboratorios Isa",119,350));
+		 agregarProducto(new Producto(10,"medicamento","Enalapril","Laboratorio Eczane Pharma",120,235));
 
 	}
 	
